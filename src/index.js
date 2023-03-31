49fac9b660c5a8bd89ddd5009e6cba700229c055
M.D. Walters <popularvsofficial@gmail.com>
2023-03-31 22:34:05 +0000

    h

--- src/index.js
+++ src/index.js
@@ -9,20 +9,16 @@ const meower = new Bot();
 const irc = new IRC.Client();
 const db = new MongoClient(process.env["I2M_MONGODB_URL"]).db("irc2meower");
 
-let channel;
-
 irc.on("registered", () => {
     irc.join(process.env.I2M_IRC_CHANNEL);
 
-    channel = irc.channel(process.env.I2M_IRC_CHANNEL);
+    const channel = irc.channel(process.env.I2M_IRC_CHANNEL);
     channel.join();
 
     irc.on("message", async (ctx) => {
-        if (ctx.nick.match(/.*Serv/i)) return;
-        const chat = await db.collection("chats").findOne({
-            irc_channel: `#${ctx.target}`
-        });
-        meower.post(`${ctx.nick}: ${ctx.message}`, chat.meower_gc);
+        if (ctx.nick.match(/.*Serv/i) || ctx.message.startsWith(`${process.env.I2M_USERNAME} `)) return;
+        const chat = await db.collection("chats").findOne({ irc: `#${ctx.target}` });
+        meower.post(`${ctx.nick}: ${ctx.message}`, chat.meower);
     });
 });
 
@@ -38,6 +34,8 @@ irc.connect({
 
 meower.onPost((username, content, origin) => {
     if (origin != null) return;
+    const channel = irc.channel(process.env.I2M_IRC_CHANNEL);
+    channel.join();
     channel.say(`${username}: ${content}`);
 });
 
