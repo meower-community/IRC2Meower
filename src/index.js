4ff60792024ba58f29f475b2b4faab05bb527485
M.D. Walters <popularvsofficial@gmail.com>
2023-03-31 11:55:45 +0000

    feat: reconnecting on both sides

--- src/index.js
+++ src/index.js
@@ -9,12 +9,10 @@ const meower = new Bot();
 const irc = new IRC.Client();
 const db = new MongoClient(process.env["I2M_MONGODB_URL"]).db("irc2meower");
 
-let channel;
-
 irc.on("registered", () => {
     irc.join(process.env.I2M_IRC_CHANNEL);
 
-    channel = irc.channel(process.env.I2M_IRC_CHANNEL);
+    const channel = irc.channel(process.env.I2M_IRC_CHANNEL);
     channel.join();
 
     irc.on("message", (ctx) => {
@@ -30,12 +28,28 @@ irc.connect({
     account: {
         account: process.env.I2M_USERNAME,
         password: process.env.I2M_PASSWORD,
-    },
+    }
 });
 
 meower.onPost((username, content, origin) => {
     if (origin != null) return;
     channel.say(`${username}: ${content}`);
+});
+
+meower.onClose(() => {
+    meower.login(process.env.I2M_USERNAME, process.env.I2M_PASSWORD);
+});
+
+irc.on("close", () => {
+    irc.connect({
+        host: process.env.I2M_IRC_URL,
+        port: 6667,
+        nick: process.env.I2M_USERNAME,
+        account: {
+            account: process.env.I2M_USERNAME,
+            password: process.env.I2M_PASSWORD,
+        }
+    });
 });
 
 meower.login(process.env.I2M_USERNAME, process.env.I2M_PASSWORD);
